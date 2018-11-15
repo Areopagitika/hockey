dd8e5a34bf6c4ed1e3e312e1ca9eaf541af0a30e
a.kondratiev <a.kondratiev@dev.digitalwand.ru>
2018-11-15 16:22:56 +0300

    initial commit

---
+++ content/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Статьи", 
+		"/content/articles/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"/content/news/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Доска объявлений", 
+		"/content/board/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
